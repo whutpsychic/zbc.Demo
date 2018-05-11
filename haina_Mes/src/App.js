668ef2d036a41aadff931e7dfce5491343d34c77
zhangbc <zhangbc@mail.taiji.com.cn>
2018-05-11 17:34:39 +0800

    haina

---
+++ haina_Mes/src/App.js
@@ -0,0 +1,14 @@
+﻿
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+
+	render() {
+		return (<div>app 节点在此</div>)
+	}
+
+}
+
+
+export default App;
---
+++ haina_Mes/src/index.js
@@ -0,0 +1,9 @@
+﻿
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+ReactDOM.render(
+	<App></App>,
+	document.getElementById('app')
+);
