1af42c2d05306f1db472e2f744daade4648a1d22
zbc <1511828529@qq.com>
2020-08-22 11:51:26 +0800

    build

---
+++ lims/codes/build/precache-manifest.d9c698a15620a8f8ecd64949c678e685.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04871e9a386ec386e7f2efc0ee03e3f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d77dd553c33045f475a",
+    "url": "./static/css/2.ed7b5974.chunk.css"
+  },
+  {
+    "revision": "dd07e1d22de1494eac9f",
+    "url": "./static/css/main.91d1baad.chunk.css"
+  },
+  {
+    "revision": "5d77dd553c33045f475a",
+    "url": "./static/js/2.481cfc38.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.481cfc38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58a2a9ae10f928c6f241",
+    "url": "./static/js/3.1ef6e434.chunk.js"
+  },
+  {
+    "revision": "bfb5395eb996382d1e1c",
+    "url": "./static/js/4.58ac60a5.chunk.js"
+  },
+  {
+    "revision": "dd07e1d22de1494eac9f",
+    "url": "./static/js/main.5f9688e4.chunk.js"
+  },
+  {
+    "revision": "34f893784a200dc17f6a",
+    "url": "./static/js/runtime-main.32ce0abc.js"
+  },
+  {
+    "revision": "b08f1f94ccba3b8b711178e53d99da2c",
+    "url": "./static/media/left-menu-shrink.b08f1f94.png"
+  },
+  {
+    "revision": "4e20fb10727b8c339a4da77735562d48",
+    "url": "./static/media/left-menu-stretch.4e20fb10.png"
+  },
+  {
+    "revision": "00f0cd4762a66015ba7577d5f31ec1e4",
+    "url": "./static/media/login-bg.00f0cd47.png"
+  },
+  {
+    "revision": "38594ebeb3be1ecbab8cc334dcb82d50",
+    "url": "./static/media/login-text.38594ebe.png"
+  },
+  {
+    "revision": "cc9accbfaa344792f7b53b9c24c780a7",
+    "url": "./static/media/position.cc9accbf.png"
+  },
+  {
+    "revision": "c41d803b991270d603b52b1def2d945e",
+    "url": "./static/media/top-icon-alert.c41d803b.png"
+  },
+  {
+    "revision": "2c711034e8c5c0304ae420fce28ccc39",
+    "url": "./static/media/top-icon-msg.2c711034.png"
+  },
+  {
+    "revision": "1d92f7df14f260d0a967fc774f40d086",
+    "url": "./static/media/top-icon-user-img.1d92f7df.png"
+  }
+]);
